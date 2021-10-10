c21badf58374a2e2daa902ad5c5d02ccfe79c126
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-10-10 14:41:48 -0400

    added inquirer; added user prompts; added validation

--- app.js
+++ app.js
@@ -1,13 +1,111 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// const pageHMTL = generatePage(name, github); 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
   
-    console.log('ReadMe complete! Check out ReadMe.html to see the output!');
-});
+//     console.log('ReadMe complete! Check out ReadMe.html to see the output!');
+// });
+
+const promptUser = readmeInputs => {
+    if (!readmeInputs) {
+        readmeInputs = [];
+    }
+
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your project title? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                console.log("Please enter a project title!");
+                return false;
+            }
+        }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please add a description of your project! (Required)",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                console.log("Please enter a project description!");
+                return false;
+            }
+        }
+        },
+        {
+            type: "input",
+            name: "installation_instructions",
+            message: "What are the steps required to install your project? (Required)",
+            validate: installation_instructionsInput => {
+                if (installation_instructionsInput) {
+                    return true;
+                } else {
+                console.log("Please describe how a user can install this project onto their local device!");
+                return false;
+            }
+        }
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Provide instructions and examples for app use! (Required)",
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                console.log("Please describe how a user can make use of this application!");
+                return false;
+            }
+        }
+        },
+        {
+            type: "input",
+            name: "contributors",
+            message: "Please list your collaborators, if any, with links to their GitHub profiles"
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "What license(s) would you like to include with this project?",
+            choices: ["Apache License 2.0", "GNU GPLv3", "MIT", "ISC"]
+        },
+        {
+            type: "input",
+            name: "Testing",
+            message: "How can users test the application?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username? (Required)",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                console.log("Please enter your GitHub username!");
+                return false;
+            }
+        }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What your email address?"
+        }
+    ]);
+};
+
+promptUser()
+    .then(readmeInputs => console.log(readmeInputs));
--- src/page-template.js
+++ src/page-template.js
@@ -6,11 +6,19 @@ const generatePage = (name, github) => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
+      <title>Professional Readme</title>
     </head>
   
     <body>
       <h1>${name}</h1>
+      <h2>Description:</h2>
+      <h2>Table of Content:</h2>
+      <h2>Installation Instructions:</h2>
+      <h2>Usage:</h2>
+      <h2>Contribution:</h2>
+      <h2>License:</h2>
+      <h2>Test Instructions:</h2>
+      <h2>Questions?</h2>
       <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
